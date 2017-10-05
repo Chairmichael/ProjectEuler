d68748ef97e6262d01799b48fdeaeba8b2252027
Jefferson V. Henry <jeffvhenry@gmail.com>
2017-10-04 22:50:55 -0500

    Progress on code

--- Python/Get_Problem.py
+++ Python/Get_Problem.py
@@ -1,13 +1,56 @@
 #!/usr/bin/env 
 # Get_Problem.py
 
+def get_description():
+    pass
+
+def select_problem(ext, num):
+    if num is not None:
+        return num
+    # path for the complete and incomplete directories
+    dirs = [starting_dir + '\\Complete', starting_dir + '\\Incomplete']
+    largest = 0
+    try:
+        for d in dirs:
+            for filename in os.listdir(d):
+                if filename.endswith(ext):
+                    num = int(filename[0:4])
+                    if int(filename[0:4]) > largest:
+                        largest = num
+    # TODO: catch dir not found and explain usage
+    # except Exception_Foo as e:
+    #     pass
+    except Exception as e:
+        raise e
+    finally:
+        return largest + 1
+
+def create_problem_file():
+    pass
+
+def get_file_ext(lang):
+    if lang is None:
+        # use the name of the root dir
+        lang = starting_dir.split('\\')[-1]
+    lang = ''.join(lang.lower().split(' ')) # lowercase and remove spaces
+    if lang == 'python': return '.py'
+    elif lang == 'java': return '.java'
+    elif lang == 'c': return '.c'
+    elif lang in ('cpp', 'c++', 'cplusplus'): return '.cpp'
+    elif lang == 'go': return '.go'
 
 def main(cl_args):
     basic_code = ("#!/usr/bin/env\n# {}.py\n\n\ndef main():\n"
                 "    pass\n\nif __name__ == '__main__':\n"
-                "    import os, sys\n    main()")
-    # print(basic_code)
-    # TODO: Get target problem
+                "    import os, sys\n    main()\n")
+    global starting_dir
+    starting_dir = os.getcwd()
+
+    extention = get_file_ext(lang=cl_args.l)
+    target = select_problem(ext=extention, num=cl_args.n)
+    print(f'\nProblem __{target}__ selected\n')
+
+    print('Connecting to projecteuler.net', end='')
     # TODO: Web-scrape to get the problem description
     
 
@@ -29,7 +72,7 @@ if __name__ == '__main__':
     parser.add_argument('-n', metavar='N', type=int, nargs='+', help=n_helptext)
 
     t_helptext = 'the programming language to initialize (default is python)'
-    parser.add_argument('-t', metavar='t', type=int, nargs='+', help=t_helptext)
+    parser.add_argument('-l', metavar='L', type=str, help=t_helptext)
 
     args = parser.parse_args()
     main(args)
