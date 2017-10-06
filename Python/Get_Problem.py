4c0fab1f1e9100b67657f555330b6a0d6e7ac325
Jefferson V. Henry <jeffvhenry@gmail.com>
2017-10-05 20:28:45 -0500

    Finished Get_Problem and started 24
    TODO: Better language support (code snippets for other languages).
    TODO: Handle website problem description better.

--- Python/Get_Problem.py
+++ Python/Get_Problem.py
@@ -10,29 +10,47 @@ def get_prob(num, url):
     print(prob_title)
     prob_desc = [textwrap.fill(p, 78) for p in prob_desc]
     prob_desc = '\n'.join(prob_desc)
-    print(prob_desc)
     return prob_title, prob_desc
 
 def select_prob(ext, num):
     if num is not None:
         return num
     # path for the complete and incomplete directories
-    dirs = [starting_dir + '\\Complete', starting_dir + '\\Incomplete']
-    largest = 0
-    try:
-        for d in dirs:
-            for filename in os.listdir(d):
-                if filename.endswith(ext):
-                    num = int(filename[0:4])
-                    if int(filename[0:4]) > largest:
-                        largest = num
-    except Exception as e:
-        raise e
-    finally:
-        return largest + 1
+    dirs = [f'{starting_dir}\\Complete', f'{starting_dir}\\Incomplete']
+    prob_count = [ ]
 
-def create_prob_file(num, content):
-    pass
+    for d in dirs:
+        for filename in os.listdir(d):
+            if filename.endswith(ext):
+                num = int(filename[0:4])
+                prob_count.append(num)
+
+    # select the gap in the problems, ex: 1,2,3,4,6 -> 5 or 1,2,3 -> 4
+    prob_count = list(set(prob_count)) # remove duplicates
+    pre = 0
+    for x in prob_count:
+        if x != pre + 1:
+            return pre + 1
+        else:
+            pre = x
+    # fallback for not having any gaps
+    else:
+        return max(prob_count) + 1
+    
+
+def create_prob_file(num, lang, content, code):
+    n = '0'*(4-len(str(num))) + str(num) # prepend zeros
+    ext = get_file_ext(lang)
+    filename = f'{n}a - {content[0]}{ext}'
+    filepath = f'{starting_dir}\\Incomplete\\{filename}'
+
+    # check if exists, as to not override an existing file
+    if not os.path.isfile(filepath):
+        with open(filepath, 'w+') as new_file:
+            new_file.write(code.format(fn=filename, desc=content[1]))
+    else:
+        print('\n\n^^ Filename already found')
+        quit()
 
 def get_file_ext(lang):
     if lang is None:
@@ -46,7 +64,7 @@ def get_file_ext(lang):
     elif lang == 'go': return '.go'
 
 def main(cl_args):
-    basic_code = ("#!/usr/bin/env\n# {fn}.py\n{desc}\n\ndef main():\n"
+    basic_code = ("#!/usr/bin/env\n# {fn}\n\n'''\n{desc}\n'''\n\n\ndef main():\n"
                 "    pass\n\nif __name__ == '__main__':\n"
                 "    import os, sys\n    main()\n")
     global starting_dir
@@ -54,22 +72,24 @@ def main(cl_args):
 
     extention = get_file_ext(lang=cl_args.l)
     target = select_prob(ext=extention, num=cl_args.n)
-    print(f'\nProblem __{target}__ selected\n')
 
+    print(f'\nProblem __{target}__ selected\n')
     print('Connecting to projecteuler.net')
+    
     prob_url = f'https://projecteuler.net/problem={target}'
     desc_text = get_prob(num=target, url=prob_url)
-    create_prob_file(num=target, content=desc_text)
+
+    create_prob_file(num=target, lang=cl_args.l,
+                     content=desc_text, code=basic_code)
 
     if cl_args.o:
-        #open problem webpage
-        pass
+        webbrowser.open_new_tab(prob_url)
     
 
 if __name__ == '__main__':
     import os, sys, argparse, textwrap
     from lxml import html
-    import requests
+    import requests, webbrowser
     
     # setup command-line arguments
     description_text = (
