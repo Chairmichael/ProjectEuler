e1abcfd6b0fe1f999086acd95dcc37dbf551a149
Jefferson V. Henry <jeffvhenry@gmail.com>
2018-05-06 18:15:23 -0500

    some thigns

--- C++/Incomplete/0003a - Largest prime factor/main.cpp
+++ C++/Incomplete/0003a - Largest prime factor/main.cpp
@@ -1,6 +1,57 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
+typedef unsigned long ulong;
+// typedef unsigned double udouble;
+
+// bool is_prime(ulong n) {
+//     if (n == 2 || n == 3) return true;
+//     if (n < 2 || n % 2 == 0) return false;
+//     if (n < 9) return true;
+//     if (n % 3 == 0) return false;
+//     ulong r = (ulong)sqrt((double)n);
+//     ulong f = 5;
+//     while (f <= r) {
+//         if (n % f == 0) return false;
+//         if (n % (f+2) == 0) return false;
+//         f += 6;
+//     }
+//     return true;
+// }
+
+bool is_prime(ulong n) {
+    bool flag = true;
+    for (int i = 2; i <= n / 2; i++) {
+        if(n % i == 0) {
+           flag = false;
+           break;
+        }
+    }
+    return flag;
+}
+
+// Largest prime factor
+unsigned long lpf(ulong n) {
+    // Get the square root to shorten the search
+    ulong s = (ulong)sqrt((double)n) + 1;
+    // Make sure s is odd
+    if (s % 2 == 0) ++s;
+    // Count down from the root by two
+    for (ulong divisor = s; divisor > 3; divisor -= 2) {
+        if (n % divisor == 0) {
+            if (is_prime(n)) {
+                return divisor;
+            }
+        }
+    }
+    return 0;
+}
 
 int main() {
-   
+    // unsigned long number = 600851475143;
+    unsigned long number = 13195;
+    // Largest prime factor
+    printf("\n%d\n", lpf(number));
+    // cout << "Largest prime factor of 600851475143: " << lpf(number) << endl;
+    return 0;
 }
---
+++ Python/Complete/0003b - Largest prime factor.py
@@ -0,0 +1,42 @@
+#/usr/bin/env 
+# 0003b - Largest prime factor.py
+
+'''
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?
+'''
+
+def is_prime(n):
+    if n == 2 or n == 3: return True
+    if n < 2 or not n % 2: return False
+    if n < 9: return True
+    if not n % 3: return False
+    root = int(n**0.5)
+    div = 5
+    while div <= root:
+        if not n % div: return False
+        if not n % (div+2): return False
+        div += 6
+    return True
+
+# Generates the prime factors 
+def prime_factors(n, direction='down'):
+    # Get n's square root
+    n_root = int(n**0.5)
+    # Make n's root odd
+    if not n_root % 2: n_root += 1
+    print(n_root)
+    if direction.lower() == 'down':
+        for div in range(n_root, 3, -2):
+            if not n % div and is_prime(n): yield div
+    if direction.lower() == 'up':
+        for div in range(3, n_root, 2):
+            if not n % div and is_prime(n): yield div
+
+
+def main():
+    [print(x) for x in prime_factors(13195)]
+
+if __name__ == '__main__':
+    import os, sys
+    main()
