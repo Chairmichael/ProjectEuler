5c73272035a071143934ebd62760bd8506d43b8f
Jefferson V. Henry <jeffvhenry@gmail.com>
2018-10-10 00:19:48 -0500

    Fixed largest prime factor function

--- C++/Incomplete/0003a - Largest prime factor/main.cpp
+++ C++/Incomplete/0003a - Largest prime factor/main.cpp
@@ -1,57 +1,51 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
-typedef unsigned long ulong;
-// typedef unsigned double udouble;
 
-// bool is_prime(ulong n) {
-//     if (n == 2 || n == 3) return true;
-//     if (n < 2 || n % 2 == 0) return false;
-//     if (n < 9) return true;
-//     if (n % 3 == 0) return false;
-//     ulong r = (ulong)sqrt((double)n);
-//     ulong f = 5;
-//     while (f <= r) {
-//         if (n % f == 0) return false;
-//         if (n % (f+2) == 0) return false;
-//         f += 6;
-//     }
-//     return true;
-// }
-
-bool is_prime(ulong n) {
-    bool flag = true;
-    for (int i = 2; i <= n / 2; i++) {
-        if(n % i == 0) {
-           flag = false;
-           break;
-        }
+bool is_prime(unsigned long n) {
+    if (n == 2 || n == 3) return true;
+    if (n < 2 || n % 2 == 0) return false;
+    if (n < 9) return true;
+    if (n % 3 == 0) return false;
+    unsigned long r = (unsigned long)sqrt((double)n);
+    unsigned long f = 5;
+    while (f <= r) {
+        if (n % f == 0) return false;
+        if (n % (f+2) == 0) return false;
+        f += 6;
     }
-    return flag;
+    return true;
 }
 
 // Largest prime factor
-unsigned long lpf(ulong n) {
+unsigned long lpf(unsigned long n) {
     // Get the square root to shorten the search
-    ulong s = (ulong)sqrt((double)n) + 1;
+    // unsigned long s = (unsigned long)sqrt((double)n) + 1;
+    unsigned long s = n / 2;
     // Make sure s is odd
-    if (s % 2 == 0) ++s;
+    if (s % 2 == 0) s += 1;
     // Count down from the root by two
-    for (ulong divisor = s; divisor > 3; divisor -= 2) {
-        if (n % divisor == 0) {
-            if (is_prime(n)) {
-                return divisor;
-            }
+    for (unsigned long divisor = s; divisor >= 3; --divisor) {
+        // printf("        %d\n", divisor);
+        if (n % divisor == 0 && is_prime(divisor)) {
+            return divisor;
         }
     }
-    return 0;
+    if (n % 2 == 0) {
+        return 2;
+    } else {
+        return 1;
+    }
 }
 
 int main() {
-    // unsigned long number = 600851475143;
-    unsigned long number = 13195;
+    unsigned long number = 600851475143;
+    // unsigned long number = 13195;
+    printf("%d: %d\n", number, lpf(number));
     // Largest prime factor
-    printf("\n%d\n", lpf(number));
+    // for (int i = 0; i < 100; ++i) {
+    //     printf("%d: %d\n", i, lpf(i));
+    // }
     // cout << "Largest prime factor of 600851475143: " << lpf(number) << endl;
     return 0;
 }
