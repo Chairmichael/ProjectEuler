b4c829f1b366ebf1f498e4ac7fe6e518de2cee5c
Jeff Henry <jeffvhenry@gmail.com>
2020-01-26 16:06:32 -0600

    yup

---
+++ C++/Incomplete/prime_factorization.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+// Largest Prime Factor
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143 ?
+
+bool isPrime(long n) {
+    
+}
+
+int main() {
+
+    return 0;
+}
